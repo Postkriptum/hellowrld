c01586f902f7bf01919cfb8351fab87adb5d30bb
Sergej P <posyakin@gmail.com>
2022-06-01 19:53:22 +0300

    minimal work(first breakfast)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-int i = 0;
+#include <iostream>
+class Uno {
+public:
+	void SetCount(int count) {
+		m_count = count;
+	}
+	int GetCount() {
+		ptrLevel = &m_count;
+		reurn* ptrLevel;
+	}
+private:
+	int m_count = 0;
+	uint32_t m_one = 0;
+	int* ptrLevel = nullptr;
+};
 
 int main(){
-	
-	
-	int one;
-		std::cout<<"Hello wrld!!! \ n";
+	Uno a;
+	a.SetCount(120);
+	std::cout << "Hello wrld!!! \ n" << a.GetCount() << std::endl;
 	return 0;
 }
